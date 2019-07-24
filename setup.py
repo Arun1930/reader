7a17f3e7aa9f537377669ab51f368282d4a69db5
arunprasad <46923726+Arun1930@users.noreply.github.com>
2019-07-24 11:44:43 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+package_name = 'reader'
+filename = package_name + '.py'
+
+
+def get_version():
+    import ast
+
+    with open(filename) as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+def get_long_description():
+    try:
+        with open('README.md', 'r') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setup(
+    name=reader,
+    version=,
+    author='xyzinnovation',
+    author_email='arun.prasad@xyzinnotech.com',
+    description='rfid for attendance',
+    url='',
+    long_description=get_long_description(),
+    py_modules=[package_name],
+    entry_points={
+        'console_scripts': [
+            'httpstat = httpstat:main'
+        ]
+    },
+    license='License :: OSI Approved :: MIT License',
+)
